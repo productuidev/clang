154a810678428aacf1d9c6a5686fe78b03eee79d
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-16 23:41:29 +0900

    Create define.c

---
+++ define.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DATA1 100
+#define DATA2 11.11
+#define DATA3 'a'
+#define DATA4 "안녕하세요"
+
+int main() { 
+    printf("DATA1:%d\n", DATA1);
+    printf("DATA2:%f\n", DATA2);  
+    printf("DATA3:%c\n", DATA3);  
+    printf("DATA4:%s\n", DATA4);  
+
+    return 0;
+}
