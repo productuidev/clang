f6fc10bc66288c266f9e90e31a8565a1155f5729
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-16 23:35:19 +0900

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+    char a1;
+    int a2;
+    float a3;
+    
+    a1 = 'c';
+    a2 = 100;
+    a3 = 11.11;
+
+    printf("a1:%c\n", a1);
+    printf("a2:%d\n", a2);
+    printf("a3:%f\n", a3);
+    
+    char a4 = 'b';
+    int a5 = 200;
+    float a6 = 22.22;
+    
+    printf("a4:%c\n", a4);
+    printf("a5:%d\n", a5);
+    printf("a6:%f\n", a6);
+    
+    int a7, a8, a9;
+    a7 = 300;
+    a8 = 400;
+    a9 = 500;
+    
+    printf("a7:%d\n", a7);
+    printf("a8:%d\n", a8);
+    printf("a9:%d\n", a9);
+    
+    int a10 = 600, a11 = 700, a12 = 800;
+    
+    printf("a10:%d\n", a10);
+    printf("a11:%d\n", a11);
+    printf("a12:%d\n", a12);
+    
+    return 0;
+}
