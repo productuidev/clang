cb97f5aecab854a0770b1722161f7cd37914ef61
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-16 23:26:31 +0900

    Create literal.c

---
+++ literal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("정수리터럴:%d\n", 100);
+    printf("실수리터럴:%f\n", 11.11);
+    printf("문자리터럴:%c\n", 'c');
+    printf("문자열리터럴:%s\n", "안녕하세요");
+    
+    return 0;
+}
