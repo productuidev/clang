ef560b8ac3313fdd9b751a0ea88570d4b8441271
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-16 23:18:31 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("char: %d\n", sizeof(char));
+    printf("unsigned char: %d\n", sizeof(unsigned char));
+    printf("signed char: %d\n", sizeof(signed char));
+    printf("int: %d\n", sizeof(int));
+    printf("unsigned int: %d\n", sizeof(unsigned int));
+    printf("short: %d\n", sizeof(short));
+    printf("unsigned short: %d\n", sizeof(unsigned short));
+    printf("long: %d\n", sizeof(long));
+    printf("unsigned long: %d\n", sizeof(unsigned long));
+    printf("float: %d\n", sizeof(float));
+    printf("double: %d\n", sizeof(double));
+    printf("long double: %d\n", sizeof(long double));
+
+    return 0;
+}
