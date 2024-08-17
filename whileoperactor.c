b7d01a27a8fc9265d799effc234d3ff95baf7b0c
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-18 00:41:10 +0900

    Create whileoperactor.c

---
+++ whileoperactor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+    int a = 10;
+    
+    while (a < 20) {
+        printf("a : %d\n", a);
+        a++;
+    }
+    
+    printf("while문 종료\n");
+    
+    return 0;
+}
