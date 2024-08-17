b3ea2470653d3924ee74b556eb5fd1fc5c0bf9cb
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-18 00:44:18 +0900

    Create loop-for.c

---
+++ loop-for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+    int i;
+    
+    for(i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+    
+    printf("for문 종료\n");
+    
+    return 0;
+}
