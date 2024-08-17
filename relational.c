251d5eb584fe0e03e8887fb7fd7fe38eafd099d8
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-17 23:51:01 +0900

    Update relational.c

--- relational.c
+++ relational.c
@@ -5,8 +5,10 @@ int main() {
     int a = 21, b = 10, c;
     
     c = a == b;
-    
     printf("a == b : %d\n", c);
+    
+    c = a != b;
+    printf("a != b : %d\n", c);
     
     return 0;
 }
