8c5226b87b0cfcdd1848232350c5e3e901a308a4
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-18 00:47:47 +0900

    Create loop-dowhile.c

---
+++ loop-dowhile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // int a = 10;
+    int a = 100;
+    
+    do {
+        printf("a : %d\n", a);
+        a++;
+    } while(a < 20);
+    
+    // a = 100;
+    
+    // while (a < 20) {
+    //     printf("a : %d\n", a);
+    // }
+    
+    // printf("while문 종료\n");
+    
+    return 0;
+}
