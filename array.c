14b8718212e279405f53c3615b0577ae8a90bae9
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-20 11:19:07 +0900

    Create array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int array1[3];
+    
+    array1[0] = 10;
+    array1[1] = 20;
+    array1[2] = 30;
+    
+    printf("array1[0] : %d\n", array1[0]);
+    printf("array1[1] : %d\n", array1[1]);
+    printf("array1[2] : %d\n", array1[2]);
+    
+    return 0;
+}
