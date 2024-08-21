1c6d54c26481a360046fbd3428fbfa9c7a14bcc9
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-08-22 00:01:49 +0900

    Create printf-float.c

---
+++ printf-float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    float a1 = 11.1111111;
+    
+    printf("%f\n", a1);
+    printf("%0.3f\n", a1);
+        
+    return 0;
+}
