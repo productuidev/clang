feac76c5280a2ef36065f02ecade4373b5260d3b
Suyeon Jung <97784036+productuidev@users.noreply.github.com>
2024-09-05 23:54:38 +0900

    Update array.c

--- array.c
+++ array.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int array1[3];
+    int array1[4];
     
     array1[0] = 10;
     array1[1] = 20;
     array1[2] = 30;
+    array1[3] = 40;
     
     printf("array1[0] : %d\n", array1[0]);
     printf("array1[1] : %d\n", array1[1]);
     printf("array1[2] : %d\n", array1[2]);
+    printf("array1[3] : %d\n", array1[3]);
     
     return 0;
 }
